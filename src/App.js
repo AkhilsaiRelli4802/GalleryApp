c257ba1d7471c0277964834d0aad0226ac6eb1c1
AkhilsaiRelli4802 <AkhilsaiRelli4802@gmail.com>
2024-03-31 12:44:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Gallery from './components/gallery';
+
+import './App.css'
+
+const App = () => <Gallery />
+
+export default App
---
+++ src/components/Thumbnailitem/index.js
@@ -0,0 +1,29 @@
+
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {itemData, onItemSelect, isSelectedItem} = props
+  const {thumbnailUrl, thumbnailAltText} = itemData
+
+  const itemStyling = `gallery-thumbnail-item ${
+    isSelectedItem ? 'selected-gallery-thumbnail' : ''
+  }`
+
+  const onClickItem = () => {
+    onItemSelect(itemData.id)
+  }
+
+  return (
+    <li className={itemStyling} onClick={onClickItem}>
+      <button type="button" className="gallery-thumbnail-button">
+        <img
+          className="gallery-thumbnail-img"
+          src={thumbnailUrl}
+          alt={thumbnailAltText}
+        />
+      </button>
+    </li>
+  )
+}
+
+export default ThumbnailItem
